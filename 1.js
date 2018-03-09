6057c59714f4867e32d1efc0a674a3e999d75814
Anastasiia <itma2011@yandex.ua>
2018-03-09 23:14:13 +0200

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,83 +1,67 @@
 
+let enAlphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+let ruAlphabet = 'йцукенгшщзхъфывапролджэячсмитьбю.';
+let uaAlphabet = 'йцукенгшщзхїфівапролджєячсмитьбю.';
+
 const keyboard = {
     layouts: {
         en: {
-            topRow: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'],
-            middleRow: ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "'"],
-            bottomRow: ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']
+            topRow: [enAlphabet.slice(0,12).split('')],
+            middleRow: [enAlphabet.slice(12,23).split('')],
+            bottomRow: [enAlphabet.slice(23,33).split('')]
         },
         ru: {
-            topRow: ['й','ц', 'у', 'к','е', 'н','г','ш','щ','з','х','ъ'],
-            middleRow: ['ф', 'ы', 'в','п', 'р', 'о', 'л', 'д', 'ж', 'э'],
-            bottomRow: ['я', 'ч', 'с','и', 'т', 'ь', 'б', 'ю', '.']
+            topRow: [ruAlphabet.slice(0,12).split('')],
+            middleRow: [ruAlphabet.slice(12,23).split('')],
+            bottomRow: [ruAlphabet.slice(23,33).split('')]
         },
         ua: {
-            topRow: ['й', 'ц', 'у','к', 'е','н', 'г','ш', 'щ', 'з', 'х', 'ї'],
-            middleRow: ['ф', 'і', 'в','п', 'р', 'о', 'л','д', 'ж', 'є'],
-            bottomRow: ['я', 'ч', 'с','и', 'т', 'ь', 'б','ю','.']
+            topRow: [uaAlphabet.slice(0,12).split('')],
+            middleRow: [uaAlphabet.slice(12,23).split('')],
+            bottomRow: [uaAlphabet.slice(23,33).split('')]
         }
     },
     langs: ['en', 'ru', 'ua'],
     currentLang: ''
 };
 
-/*Написать скрипт который на старте спрашивает пользователя 
-какой язык он хочет использовать на тренажере. Это обычный prompt
- в котором написано en-0, ru-1, ua-2. Пользователь вводит 0, 1 
- или 2, если введено другое значение то вывести alert о том что 
- был выбран не доступный язык и повторить prompt до того момента 
- пока не будет введено подходящее значание языка или нажат cancel
- . При cancel прекратить выполнение скрипта. 
- Результат выбора языка пользователем записать в обьект keyboard 
- в поле currentLang как строку, 0 это en, 1 это ru, 2 это ua.
- Модифицировать функцию getRandCharInAlph() так, чтобы она возвращала 
- случайную букву из выбраного пользователем алфавита.*/
-
  document.querySelector('#btn1').onclick=function changeLanguage(){
  	let prom = prompt('Выберете язык: en-0, ru-1, ua-2');
 
  	switch(prom) {
  		case '0':keyboard.currentLang = 'en';
- 		console.log('Выбран английский язык');
+ 		console.log('Вы выбрали английский язык');
  		break;
 
  		case '1':keyboard.currentLang = 'ru';
- 		console.log('Выбран русский язык');
+ 		console.log('Вы выбрали русский язык');
  		break;
 
  		case '2':keyboard.currentLang = 'ua';
- 		console.log('Выбран украниский язык');
+ 		console.log('Вы выбрали украинский язык');
  		break;
 
  		case null:
- 		console.log('false');
+ 		console.log('Пользователь закрыл окно');
  		break;
 
  		default:
- 		alert('Выбран недоступный язык');
+ 		alert('Поле неккоректно заполнено');
  		changeLanguage();
  	}
  }
 
 document.querySelector('#btn2').onclick=function getRandCharInAlph() {
-	if (keyboard.currentLang === 'en'){
-		console.log(`Случайная буква из английского алфавита - ${keyboard['layouts']['en'][Math.floor(Math.random()*12)]} `);
+	if (keyboard.currentLang === 'ru'){
+		console.log(`Случайная буква из русского алфавита - ${ruAlphabet[Math.floor(Math.random()*33)]}`);
 	}
 
-	if (keyboard.currentLang === 'ru'){
-		console.log(`Случайная буква из русского алфавита - ${keyboard['layouts']['ru']['topRow'][Math.floor(Math.random()*12)]}`);
+	if (keyboard.currentLang === 'en'){
+		console.log(`Случайная буква из английского алфавита - ${enAlphabet[Math.floor(Math.random()*33)]}`);
 	}
 
 	if (keyboard.currentLang === 'ua'){
-		console.log(`Случайная буква из украинского алфавита - ${keyboard['layouts']['ua']['topRow'][Math.floor(Math.random()*12)]}`);
+		console.log(`Случайная буква из украинского алфавита - ${uaAlphabet[Math.floor(Math.random()*33)]}`);
 	}
-
 }
 
-
-
-
-
-//  document.querySelector('#btn2').onclick=function getRandCharInAlph() {
-// 	return console.log(`Случайная буква из всего алфавита - ${alphabet[Math.floor(Math.random()*33)]}`);
-// }
